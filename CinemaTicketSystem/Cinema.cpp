7a35e85155379a9eb04fd2ef611fc91484f2bef7
samab <samab@SAMAB>
2023-04-20 17:17:31 -0400

    Small adds to Cinema, Movie and ERoomType

--- CinemaTicketSystem/Cinema.cpp
+++ CinemaTicketSystem/Cinema.cpp
@@ -1,1 +1,51 @@
 #include "Cinema.h"
+
+//DEFINITIONS
+//Declare constructor and destructor
+Cinema::Cinema(int roomAmount) 
+{
+	try
+	{
+		this->roomAmount = roomAmount;
+	}
+	catch (...)
+	{
+
+	}
+}
+
+Cinema::~Cinema() 
+{
+
+}
+
+//Declare getters and setters
+int Cinema::GetRoomAmount() 
+{
+	try
+	{
+		return roomAmount;
+	}
+	catch (...)
+	{
+
+	}
+}
+
+void Cinema::SetRoomAmount(int newAmount) 
+{
+	try
+	{
+		roomAmount = newAmount;
+	}
+	catch (...)
+	{
+
+	}
+}
+
+//Methode that generates a schedule for every movie
+void Cinema::GenerateSchedule() 
+{
+
+}
--- CinemaTicketSystem/Movie.cpp
+++ CinemaTicketSystem/Movie.cpp
@@ -5,12 +5,19 @@
 //Declare constructor and destructor
 Movie::Movie(string title, float length, Genre genre, string director, string mainActor, string mainActress)
 {
-	this->title = title;
-	this->length = length;
-	this->genre = genre;
-	this->director = director;
-	this->mainActor = mainActor;
-	this->mainActress = mainActress;
+	try
+	{
+		this->title = title;
+		this->length = length;
+		this->genre = genre;
+		this->director = director;
+		this->mainActor = mainActor;
+		this->mainActress = mainActress;
+	}
+	catch (...)
+	{
+
+	}
 }
 
 Movie::~Movie() 
